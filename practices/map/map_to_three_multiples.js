c6df0be7f6cef5de097f795b62908d4a30939fb2
hoyhe <hoyhe@thoughtworks.com>
2018-09-04 17:31:21 +0800

    [Herb] Finish map

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,6 +1,6 @@
 'use strict';
-var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+var map_to_three_multiples = function (collections) {
+  return collections.map(num => num * 3);
 };
 
 module.exports = map_to_three_multiples;
--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,19 @@
 'use strict';
+function convert(num) {
+  var result = [];
+  while (num) {
+    var t = num % 26;
+    if (!t) {
+      t = 26;
+      --num;
+    }
+    result.push(String.fromCodePoint(t + 96));
+    num = ~~(num / 26);
+  }
+  return result.reverse().join('');
+}
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map(num=>convert(num));
 };
 
 module.exports = number_map_to_word;

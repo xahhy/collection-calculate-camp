4d2696a94ddb8e408b20d7e6d1892783652fa411
hoyhe <hoyhe@thoughtworks.com>
2018-09-04 21:12:13 +0800

    [Herb] Finish section_2 except practice 3

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,11 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let countMap=[];
+  let resultMap=[];
+  collection.forEach(item=> countMap[item] = countMap[item] ? ++countMap[item] : 1);
+  for (key in countMap) {
+    resultMap.push({key: key, count: countMap[key]})
+  }
+  return resultMap;
 }
 
 module.exports = count_same_elements;

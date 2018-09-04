78de37375eae441f46684438d840feb1b02a6d6e
Herb <hoyhe@thoughtworks.com>
2018-09-04 13:31:51 +0800

    [Herb] collection practise.

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function collect_last_element(collection) {
-  //在这里写入代码
+  return collection.pop(-1);
 }
 
 module.exports = collect_last_element;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  if(number_a > number_b){
+    let i=0;
+    return [...Array(number_a+1)].map(_=>i++).reverse().filter(item=>item%2===0 && item >= number_b)
+  }else{
+    let i=0;
+    return [...Array(number_b+1)].map(_=>i++).filter(item=>item%2===0 && item >= number_a)
+  }
 }
 
 module.exports = get_integer_interval_2;
